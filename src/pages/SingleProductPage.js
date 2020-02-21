248d0c31ade44b5f9dbb2af8397d3c409bb56da4
mohdraqif <mohdraqif94@gmail.com>
2020-02-22 01:32:55 +0530

    Init commit

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -10,9 +10,9 @@ export default function SingleProductPage() {
       <ProductConsumer>
         {value => {
           const { singleProduct, addToCart, loading } = value;
+          console.log(singleProduct);
 
           if (loading) {
-            console.log("hello from loading");
             return <h1>product loading....</h1>;
           }
           const {
@@ -23,20 +23,21 @@ export default function SingleProductPage() {
             title,
             image
           } = singleProduct;
+          console.log(image)
           return (
             <section className="py-5">
               <div className="container">
                 <div className="row">
                   <div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
                     <img
-                      src={`../${image}`}
+                      src={`${image}`}
                       // src={image}
                       alt="single product"
                       className="img-fluid"
                     />
                   </div>
                   <div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
-                    <h5 className="text-title mb-4">model :{title} </h5>
+                    <h5 className="text-title mb-4">model : {title} </h5>
                     <h5 className="text-capitalize text-muted mb-4">
                       company : {company}
                     </h5>
